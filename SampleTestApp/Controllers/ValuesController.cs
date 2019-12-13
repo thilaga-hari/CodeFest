aa266e587385c95a5d7d700be05c9faef38dd327
thilaga-hari <thilaga.yuva@gmail.com>
2019-12-13 08:46:53 +0530

    adding to code fest

---
+++ SampleTestApp/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleTestApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        // GET api/values/5
+        [HttpGet ("{value}")]
+        public string Get([FromBody] double value)
+        {
+            double f;
+            f = value * 9 / 5 + 32;
+            return (value + "celsius in Farenheit is:" + f);
+
+        }
+    }
+}
